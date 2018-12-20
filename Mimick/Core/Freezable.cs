27369b2d4a1760dbea621ec9484bf760f6155241
Chris Copeland <christopher.copeland@radiuspaymentsolutions.com>
2018-12-20 14:41:25 +0000

    Added new Freezable helper class Added IsTestProject property to the test project Fixed the dotnet test not targetting the tests project directly

---
+++ Mimick/Core/Freezable.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mimick
+{
+    /// <summary>
+    /// A class containing methods for freezing objects, and checking whether objects are frozen.
+    /// </summary>
+    public static class Freezable
+    {
+        /// <summary>
+        /// Gets an <see cref="IFreezable"/> reference from a provided target object.
+        /// </summary>
+        /// <param name="target">The target object.</param>
+        /// <returns>An <see cref="IFreezable"/> value.</returns>
+        /// <exception cref="ArgumentNullException">If the target object is <c>null</c>.</exception>
+        /// <exception cref="ArgumentException">If the target object is not a freezable type.</exception>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static IFreezable GetFreezable(object target)
+        {
+            if (target == null)
+                throw new ArgumentNullException("target");
+
+            return target as IFreezable ?? throw new ArgumentException($"The object type {target.GetType().FullName} does not implement the IFreezable interface");
+        }
+
+        /// <summary>
+        /// Freeze a provided object.
+        /// </summary>
+        /// <param name="target">The target object.</param>
+        /// <exception cref="ArgumentNullException">If the target object is <c>null</c>.</exception>
+        /// <exception cref="ArgumentException">If the target object is not a freezable type.</exception>
+        /// <exception cref="FrozenException">If the target object has already been frozen.</exception>
+        public static void Freeze(object target) => GetFreezable(target).Freeze();
+
+        /// <summary>
+        /// Gets whether a provided object has been frozen.
+        /// </summary>
+        /// <param name="target">The target object.</param>
+        /// <returns><c>true</c> if the object has been frozen; otherwise, <c>false</c>.</returns>
+        /// <exception cref="ArgumentNullException">If the target object is <c>null</c>.</exception>
+        /// <exception cref="ArgumentException">If the target object is not a freezable type.</exception>
+        public static bool IsFrozen(object target) => GetFreezable(target).IsFrozen;
+    }
+}
